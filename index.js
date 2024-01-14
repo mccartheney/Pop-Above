c2fb20c857be767b19217d75f74a7a99ef43f02d
Mccartheney Mendes <mccartheney@hotmail.com>
2024-01-14 01:33:10 +0000

    js finalized

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 export default class popAbove {
     //elementId is the id of the element which will have a pop up above 
-    constructor (elementID, content, pxAbove) {
+    constructor (elementID, content, pxAbove, backgroundColor) {
         //get the element by id
         this.elementID = document.querySelector (`#${elementID}`);
         //content is the html content which will apear inside the pop up
@@ -9,6 +9,8 @@ export default class popAbove {
         this.pxAbove = pxAbove;
         //popUp is the popUp which will appear to user
         this.popUp = document.createElement("div");
+        //background color for popUp
+        this.backgroundColor = backgroundColor;
     }
 
     //with this user can change the style easily withb css
@@ -20,16 +22,21 @@ export default class popAbove {
             let square = document.createElement ("div");
             square.style.width = "20px";
             square.style.height = "20px";
-            square.style.backgroundColor = this.elementID.style.backgroundColor;
             square.style.position = "absolute";
             square.style.bottom = "-10px";
             square.style.zIndex = "-2";
             square.style.transform = "rotate(45deg)";
+            square.style.left = " calc(50% - 10px)";
+            square.style.backgroundColor = this.backgroundColor;
             
             //adding the content to popUp
             this.popUp.innerHTML = this.content
             //setting the amount of pixels the popUp will be about the div
             this.popUp.style.top = `-${this.pxAbove}px`;
+            //setting dispçay to absoluto is essecial for correct funcionality
+            this.popUp.style.position = `absolute`;
+            //background color to popUp
+            this.popUp.style.backgroundColor = this.backgroundColor;
             //adding the popUP to the div
             this.elementID.appendChild(this.popUp);
             //adding square to the div
